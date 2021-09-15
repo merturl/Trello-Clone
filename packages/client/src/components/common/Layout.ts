01e57488b549488282b91028437047a713beac49
merturl <whdgus963@naver.com>
2021-09-16 04:56:51 +0900

    Add layout component to wrap other component

---
+++ packages/client/src/components/common/Layout.ts
@@ -0,0 +1,28 @@
+import Component from "../core/Component.js";
+
+interface Props {
+  className: string;
+}
+
+interface State {}
+
+class Layout extends Component<Props, State> {
+  $target: HTMLDivElement;
+
+  constructor($parent: HTMLElement, props: Props) {
+    super($parent, props);
+    this.state = {};
+    document.createDocumentFragment;
+    this.$target = document.createElement("div");
+    this.$parent.appendChild(this.$target);
+    this.render();
+  }
+  render = () => {
+    this.$target.innerHTML = "";
+
+    const { className } = this.props;
+    this.$target.className = className;
+  };
+}
+
+export default Layout;
